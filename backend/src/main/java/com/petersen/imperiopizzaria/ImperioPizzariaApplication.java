065d11181b1a146df9aa7b625292612d9acedb55
Petersen <petersen@localhost.localdomain>
2024-05-08 12:06:01 -0300

    Project created

---
+++ backend/src/main/java/com/petersen/imperiopizzaria/ImperioPizzariaApplication.java
@@ -0,0 +1,13 @@
+package com.petersen.imperiopizzaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImperioPizzariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImperioPizzariaApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/petersen/imperiopizzaria/ImperioPizzariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petersen.imperiopizzaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImperioPizzariaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
