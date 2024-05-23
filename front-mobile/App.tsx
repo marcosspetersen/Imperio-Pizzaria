64a1c65d4118c2a14c6afe99a190e080f0a8d319
marcosspetersen <marcos_petersen@hotmail.com>
2024-05-23 20:39:14 -0300

    Header and home done

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,10 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
+import { StatusBar } from 'expo-status-bar';
+import Header from './src/Header';
+import Home from './src/Home';
 
 export default function App() {
+  let [fontsLoaded, fontError] = useFonts({
+    OpenSans_400Regular,
+    OpenSans_700Bold
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Header />
+      <Home/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +27,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
